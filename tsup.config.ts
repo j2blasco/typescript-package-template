1cca7dc0a3cffb87c5fac90c9948569129c03412
blasco <blascoburguillos@gmail.com>
2025-07-14 18:19:01 +0200

    refactor: update tsconfig and tsup configuration for consistency and clarity

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
+  target: 'ES2022',
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
+  outDir: 'dist',
+  tsconfig: 'tsconfig.json',
   clean: true,
   dts: true,
   sourcemap: true,
-  outDir: 'dist',
-  tsconfig: 'tsconfig.json',
-  target: 'es2020',
 });
