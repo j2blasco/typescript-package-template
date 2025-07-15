c575248c636a6fa69d11abf81bd225e50755f44f
blasco <blascoburguillos@gmail.com>
2025-07-15 20:11:57 +0200

    refactor: simplify jest configuration by removing unnecessary ESM settings

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,11 @@
-import type { JestConfigWithTsJest } from 'ts-jest'
+import type { JestConfigWithTsJest } from 'ts-jest';
 
 const config: JestConfigWithTsJest = {
-  extensionsToTreatAsEsm: ['.ts'],
   verbose: true,
-  preset: 'ts-jest/presets/default-esm',
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', { useESM: true }]
-  },
-  testMatch: [
-    '**/*.test.ts',
-    '!**/*.utils.test.ts'
-  ],
-  testPathIgnorePatterns: ['./dist']
-}
+  testMatch: ['**/*.test.ts', '!**/*.utils.test.ts', '!**/*.generic.test.ts'],
+  testPathIgnorePatterns: ['./dist'],
+};
 
-export default config
+export default config;
